9b18d95be7bb55bd7e7aec8a234c9b46b3e1a22b
Louis <louis@embedded-creations.com>
2012-05-12 23:20:37 -0700

    Added hardware configurations for tiny85 mode, clarified pin definitions to match pin interrupt requirements, made pinchange interrupt follow pin definition automatically
    tested with hardware configuration 2, works

--- firmware/bootloaderconfig.h
+++ firmware/bootloaderconfig.h
@@ -42,21 +42,51 @@ names BOOTLOADER_INIT and BOOTLOADER_CONDITION for this functionality. If
 these macros are defined, the boot loader uses them.
 */
 
+#define TINY85_HARDWARE_CONFIG_1 	1
+#define TINY85_HARDWARE_CONFIG_2 	2
+
 /* ---------------------------- Hardware Config ---------------------------- */
+#define HARDWARE_CONFIG 		TINY85_HARDWARE_CONFIG_2
 
 #define USB_CFG_IOPORTNAME      B
 /* This is the port where the USB bus is connected. When you configure it to
  * "B", the registers PORTB, PINB and DDRB will be used.
  */
-#define USB_CFG_DMINUS_BIT      0
+
+#ifndef __AVR_ATtiny85__
+#	define USB_CFG_DMINUS_BIT      0
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
-#define USB_CFG_DPLUS_BIT       2
+#	define USB_CFG_DPLUS_BIT       2
 /* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
  * This may be any bit in the port. Please note that D+ must also be connected
  * to interrupt pin INT0!
  */
+#endif
+ 
+#if (defined __AVR_ATtiny85__) && (HARDWARE_CONFIG == TINY85_HARDWARE_CONFIG_1)
+#	define USB_CFG_DMINUS_BIT      0
+/* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
+ * This may be any bit in the port, but must be configured as a pinchange interrupt (see below)
+ */
+#	define USB_CFG_DPLUS_BIT       2
+/* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
+ * This may be any bit in the port.
+ */
+ #endif
+ 
+#if (defined __AVR_ATtiny85__) && (HARDWARE_CONFIG == TINY85_HARDWARE_CONFIG_2)
+#	define USB_CFG_DMINUS_BIT      3
+/* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
+ * This may be any bit in the port, but must be configured as a pinchange interrupt (see below)
+ */
+#	define USB_CFG_DPLUS_BIT       4
+/* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
+ * This may be any bit in the port.
+ */
+ #endif
+
 #define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
 /* Clock rate of the AVR in MHz. Legal values are 12000, 16000 or 16500.
  * The 16.5 MHz version of the code requires no crystal, it tolerates +/- 1%
@@ -165,9 +195,9 @@ these macros are defined, the boot loader uses them.
 #       define BOOTLOADER_CAN_EXIT 1
 #   endif
 
-// setup interrupt for Pin Change on PB0
+// setup interrupt for Pin Change for D-
 #   define USB_INTR_CFG            PCMSK
-#   define USB_INTR_CFG_SET        (1 << PCINT0)
+#   define USB_INTR_CFG_SET        (1 << USB_CFG_DMINUS_BIT)
 #   define USB_INTR_CFG_CLR        0
 #   define USB_INTR_ENABLE         GIMSK
 #   define USB_INTR_ENABLE_BIT     PCIE
